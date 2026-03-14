09ff8839f873256ed44c1f31fc7af92aa3af55c4
ksutsova <kkksenya.kkkuzya@mail.ru>
2026-03-14 14:21:56 +0300

    ifp

---
+++ ImageFilterPipeline/src/Pipeline.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>  // std::ostream — для вывода лога выполнения
+#include <memory>    // std::unique_ptr — умный указатель с эксклюзивным владением
+#include <string>    // std::string
+#include <vector>    // std::vector — динамический массив фильтров
+
+#include "Filter.hpp"
+#include "Image.hpp"
+
+namespace ifp {
+
+
+
+}  // namespace ifp
